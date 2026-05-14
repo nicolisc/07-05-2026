0ddc4fb767a92968e396fd98ea65a6f4e8bac5b3
nicolisc <nicole.scheis@escola.pr.gov.br>
2026-05-14 09:49:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+const botoes=documen.querySelectionAll(".botao")
+for (let i=0;i<botoes.length;i++){
+    botoes[i].onclick=function (){
+        for(let j=0;<botoes.length;j++){
+            botoes [j].classList.remove("ativo")
+        }
+        botoes [i].classList.add("ativo")
+    }
+}
